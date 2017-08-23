16c5a00a07856d064a5513485581d2ed065dfcfb
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2017-08-23 17:29:10 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const targetFile = './watched-file.json';
+const express = require('express');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+	console.log(`${process.pid} - Master started`);
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+} else {
+	console.log(`${process.pid} - Worker started`);
+}
+
+fs.watch(targetFile, (eventType, filename) => {
+  fs.readFile(targetFile, 'utf8', function (err, data) {
+    if (err) throw err;
+    process.env.CONTENT = data;
+    console.log(`${process.pid} - ${process.env.CONTENT}`);
+  });
+});
